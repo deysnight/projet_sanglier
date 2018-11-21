573207c376c2d3819a3ab0dd4aed94478a7e4b75
Unknown <louison.harizi@epitech.eu>
2018-11-21 15:04:51 +0100

    connection to sqlite DB

---
+++ serveur/chasse_serv/chasse_serv/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chasse_serv.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            using (var connection = new SQLiteConnection("" + new SQLiteConnectionStringBuilder {DataSource = "test.db"}))
+            {
+                connection.Open();
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    var insertCommand = connection.CreateCommand();
+                    insertCommand.Transaction = transaction;
+                    insertCommand.CommandText = "INSERT INTO \"index\" (nom) VALUES (\"bite2\"),(\"bite\");";
+                    insertCommand.ExecuteNonQuery();
+
+                    var selectCommand = connection.CreateCommand();
+                    selectCommand.Transaction = transaction;
+                    selectCommand.CommandText = "SELECT * FROM \"index\"";
+                    using (var reader = selectCommand.ExecuteReader())
+                    {
+                        var items = new Dictionary<object, Dictionary<string, object>>();
+                        while (reader.Read())
+                        {
+                            var item = new Dictionary<string, object>(reader.FieldCount - 1);
+                            for (var i = 1; i < reader.FieldCount; i++)
+                            {
+                                item[reader.GetName(i)] = reader.GetValue(i);
+                            }
+                            items[reader.GetValue(0)] = item;
+                        }
+                        var json = Newtonsoft.Json.JsonConvert.SerializeObject(items, Newtonsoft.Json.Formatting.Indented);
+                        Console.Write(json);
+                    }
+
+                    transaction.Commit();
+                }
+            }
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ serveur/chasse_serv/chasse_serv/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chasse_serv
+{
+    public class Utils
+    {
+
+    }
+}
